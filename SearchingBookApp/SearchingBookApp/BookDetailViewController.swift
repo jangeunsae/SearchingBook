4c145ec1eee0bb8862f3309a3f665a15f6892a56
jangeunsae <jesbio@naver.com>
2025-07-30 15:18:11 +0900

    add: BookDetailViewController
    책 상세 내용 페이지 구현

---
+++ SearchingBookApp/SearchingBookApp/BookDetailViewController.swift
@@ -0,0 +1,34 @@
+//
+//  BookDetailViewController.swift
+//  SearchingBookApp
+//
+//  Created by 장은새 on 7/30/25.
+//
+
+import UIKit
+
+class BookDetailViewController: UIViewController {
+    
+    let bookDetailTableView = UITableView()
+    let detailTitleLabel = UILabel()
+    let datailAuthorLabel = UILabel()
+    let imageView = UIImageView()
+    let detailPriceLabel = UILabel()
+    let detailContentLabel = UILabel()
+    let escapeButton = UIButton()
+    let cartButton = UIButton()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.addSubview(bookDetailTableView)
+        view.addSubview(detailTitleLabel)
+        view.addSubview(datailAuthorLabel)
+        view.addSubview(imageView)
+        view.addSubview(detailPriceLabel)
+        view.addSubview(detailContentLabel)
+        view.addSubview(escapeButton)
+        view.addSubview(cartButton)
+
+    }
+
+}
